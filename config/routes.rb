cfdf3e8a6913bf2d89f79b75a200a8f33f1d2ec3
Barnabas Chan <barny@post.com>
2016-08-01 12:30:14 +0800

    add link to about page to nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help'
+  get '/home', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
 
   resources :bijous
   resources :rockhounds
